f27ee29ff93d6d1bec3b1fedf050e0655e70cb80
Krystle <krystle@Krystles-MBP.lan>
2025-02-05 23:10:53 -0800

    WIP

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.get('/', (req, res) => {
     res.send('Fashion API is running...');
 })
 
+// Error-handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+  });
+
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
